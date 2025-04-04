2a7b0dba7b3699adabebd1f39f31f02049793d58
Sivakumar <sivakumarec99@gmail.com>
2025-04-04 18:53:44 +0530

    Commit Link View Menu View FromView Created

---
+++ Interview/Interview/SwiftUI/View/Component/Link/LinkView.swift
@@ -0,0 +1,8 @@
+//
+//  LinkView.swift
+//  Interview
+//
+//  Created by JIDTP1408 on 04/04/25.
+//
+
+import Foundation
---
+++ Interview/Interview/SwiftUI/View/Component/Menu/MenuView.swift
@@ -0,0 +1,8 @@
+//
+//  MenuView.swift
+//  Interview
+//
+//  Created by JIDTP1408 on 04/04/25.
+//
+
+import Foundation
